c917ee9e9ffd60a003985e212fd8994d8f745a90
Viniciuscoding <vin.idem@gmail.com>
2023-02-05 11:08:00 -0500

    Create non_english_words.py

---
+++ non_english_words.py
@@ -0,0 +1,12 @@
+'''
+Code that helps find any word that is not in the English dictionary from the NLTK library.
+'''
+
+import nltk
+
+nltk.download("words")
+
+class NonEnglishWords:
+    
+    def 
+
